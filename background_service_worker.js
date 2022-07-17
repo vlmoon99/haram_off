7a571a8ae58e89e92a79c09681a57a092e7d2d79
vlmoon99 <vlmoon@protonmail.com>
2022-07-17 18:26:29 +0300

    add_listners_for_video_capture

--- background_service_worker.js
+++ background_service_worker.js
@@ -1,5 +1,27 @@
+function startRecording() {
+    chrome.tabs.getSelected(null, function(tab) {
+      var constraints = {
+        audio: false,
+        video: true,
+        videoConstraints: {
+          mandatory: {
+            chromeMediaSource: 'tab',
+            maxWidth: 1280,
+            maxHeight: 720,
+            maxFrameRate: 15,
+            minAspectRatio: 1.77,
+            googLeakyBucket: true,
+            googTemporalLayeredScreencast: true
+          }
+        }
+      };
+      chrome.tabCapture.capture(constraints, handleCapture);
+    });
+  }
+
+function handleCapture(MediaStream) {}
 
 
-chrome.runtime.onInstalled.addListener(() => {
-    console.log("Hello World");
-});
+chrome.tabs.onActivated.addListener(function(tab){
+    coneole.log(tab.tabId);
+  });
